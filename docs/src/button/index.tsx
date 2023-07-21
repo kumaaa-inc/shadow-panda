334ca1ce716cec17e4fc1057715ebf9099299847
nanopx <0nanopx@gmail.com>
2023-07-22 04:47:48 +0900

    Fix Button component example to match shadcn/ui

--- docs/src/button/index.tsx
+++ docs/src/button/index.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { cx } from '@shadow-panda/styled-system/css'
 import {
   button,
   type ButtonVariantProps,
 } from '@shadow-panda/styled-system/recipes'
 
-export interface ButtonProps extends ButtonVariantProps {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    ButtonVariantProps {
+  asChild?: boolean
   children: React.ReactNode
 }
 
-export const Button = ({ variant, size, children }: ButtonProps) => {
-  return (
-    <button
-      className={button({
-        variant,
-        size,
-      })}
-    >
-      {children}
-    </button>
-  )
-}
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button'
+
+    return (
+      <Comp
+        className={cx(button({ variant, size }), className)}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
+
+Button.displayName = 'Button'
