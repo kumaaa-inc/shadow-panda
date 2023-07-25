e823e12c066f685e02e06717e0dd970dbfb0ed8c
nanopx <0nanopx@gmail.com>
2023-07-25 23:04:42 +0900

    Feat: Alert component

--- packages/preset/src/recipes/index.ts
+++ packages/preset/src/recipes/index.ts
@@ -1,9 +1,11 @@
 import * as typography from './typography'
 import * as accordion from './accordion'
+import * as alert from './alert'
 import * as button from './button'
 
 export const recipes = {
   ...typography,
   ...accordion,
+  ...alert,
   ...button,
 }
