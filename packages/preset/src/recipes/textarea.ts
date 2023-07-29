d19e341577ced2f907f4eda4a68c302612db7e78
nanopx <0nanopx@gmail.com>
2023-07-29 12:18:01 +0900

    Add Textarea component

---
+++ packages/preset/src/recipes/textarea.ts
@@ -0,0 +1,35 @@
+import { defineRecipe } from '@pandacss/dev'
+
+export const textarea = defineRecipe({
+  name: 'textarea',
+  description: 'Styles for the Textarea component',
+  base: {
+    display: 'flex',
+    minH: '80px',
+    w: 'full',
+    rounded: 'md',
+    border: 'input',
+    bg: 'transparent',
+    px: '3',
+    py: '2',
+    fontSize: 'sm',
+    focusRingOffsetColor: 'background',
+
+    _placeholder: {
+      color: 'muted.foreground',
+    },
+
+    _focusVisible: {
+      outline: '2px solid transparent',
+      outlineOffset: '2px',
+      focusRingWidth: '2',
+      focusRingColor: 'ring',
+      focusRingOffsetWidth: '2',
+    },
+
+    _disabled: {
+      cursor: 'not-allowed',
+      opacity: '0.5',
+    },
+  },
+})
