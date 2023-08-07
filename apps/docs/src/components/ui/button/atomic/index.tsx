90bb14425dc58596b197982f0c1516cd917d7404
nanopx <0nanopx@gmail.com>
2023-08-07 10:54:39 +0900

    docs: Add atomic recipe version of button

--- apps/docs/src/components/ui/button/atomic/index.tsx
+++ apps/docs/src/components/ui/button/atomic/index.tsx
@@ -116,20 +116,19 @@ export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
     asChild?: boolean
   }
 
-const Button = React.forwardRef<
-  HTMLButtonElement,
-  ButtonProps & ButtonVariantProps
->(({ className, variant, size, asChild = false, ...props }, ref) => {
-  const Comp = asChild ? Slot : 'button'
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button'
 
-  return (
-    <Comp
-      className={cx(buttonVariants({ variant, size }), className)}
-      ref={ref}
-      {...props}
-    />
-  )
-})
+    return (
+      <Comp
+        className={cx(buttonVariants({ variant, size }), className)}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
 
 Button.displayName = 'Button'
 
